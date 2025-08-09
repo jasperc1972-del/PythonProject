81ccee0eca1782d7e21edc2d962559f384a25c05
jasperc1972-del <jasperc1972@gmail.com>
2025-08-09 23:29:19 +0800

    python variable and types
    python variable and types

---
+++ PythonProject/variable.py
@@ -0,0 +1,18 @@
+#integer
+age=20
+
+#float
+score=89.5
+#隱性type
+print(age+score)
+print(f"我的分數{score}")
+
+#string
+name='Mary'
+print(name)
+print(f"我的名字-{name}")
+
+#boolean true or false
+is_online=True
+
+print(f"上線了嗎 {is_online}")
