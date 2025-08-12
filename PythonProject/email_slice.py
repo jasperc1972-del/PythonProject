36cfd2f7bc39373327e486fc9faa5ca996a30fd8
Jasper <jasperc1972@gmail.com>
2025-08-12 14:27:25 +0800

    Introducing the string format
    Introducing the string format

---
+++ PythonProject/email_slice.py
@@ -0,0 +1,7 @@
+# Email字串分析
+
+email="jasperc1972@gmail.com"
+index=email.find("@")
+print(index)
+print(email[0:index])
+print(email[index+1:])
---
+++ PythonProject/f_string.py
@@ -0,0 +1,10 @@
+# f_string 字串格式化
+
+price1=3.5471
+price2=5.74132
+price3=4.78995
+
+#小數點精確度
+print(f"價格1: {price1 :.2f}\n"
+       f"價格2: {price2:.2f} \n"
+       f"價格3: {price3 :.2f} ")
