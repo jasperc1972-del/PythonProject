d8a551420231b428403017dc323675591e2ab1be
Jasper <jasperc1972@gmail.com>
2025-08-15 23:27:51 +0800

    while loop  application
    while loop  application

---
+++ PythonProject/whileLoop.py
@@ -0,0 +1,40 @@
+#while loop
+
+# name=input("請輸入你的名字:")
+# if  name =="":
+#     print("你沒有輸入名字")
+# else:
+#     print(f"你好,{name}")
+
+
+
+# name=input("請輸入你的名字:")
+# while  name =="":
+#     name=input("請輸入你的名字:")
+#   print(f"你好,{name}")
+
+
+
+# name=""
+# while  name =="":
+#     name=input("請輸入你的名字:")
+# print(f"你好,{name}")g
+
+
+
+# food=input("你喜歡吃的食物是什麼:")
+# while food !="q":
+#     print(f"你喜歡吃的食物是{food}")
+#     food=input("你喜歡吃的食物是什麼:")
+# print("good bye !")
+
+#sample3
+num=int(input("輸入1到10之間的數字:"))
+while num<1 or num>10:
+    print(f"你輸入的數字{num}是無效的")
+    num = int(input("輸入1到10之間的數字:"))
+print(f"你輸入了{num}")
+
+
+
+
