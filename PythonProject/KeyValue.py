c5b072b52ff5a9143f42f3d5613351c82cc2d1ef
Jasper <jasperc1972@gmail.com>
2025-08-17 12:32:23 +0800

    key value application
    key value application

---
+++ PythonProject/KeyValue.py
@@ -0,0 +1,11 @@
+# key value dictionary
+
+# my_dict={"a":1,"b":2,"c":3,"d":4}
+# for z in my_dict:
+#     print(z)
+
+my_dict={"a":1,"b":2,"c":3,"d":4}
+for Xkey,Xvalue in my_dict.items():
+    print("key:",Xkey)
+    print("value:",Xvalue)
+
