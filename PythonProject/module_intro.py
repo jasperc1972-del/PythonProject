e675d1a2ddb0fff6b480d31f4ee9313276bdb864
Jasper <jasperc1972@gmail.com>
2025-08-26 10:05:40 +0800

    module introduce
    module introduce

---
+++ PythonProject/module_intro.py
@@ -0,0 +1,22 @@
+#python 中的模組
+import math as m
+# help(math)
+
+# print(m.pi)
+# print(m.pow(3,2))#3的2次方
+# print(m.pow(2,2))#2的2次方
+
+
+# ceil 無條件進位
+# floor 無條件捨去
+# round 四捨五入
+
+n=16.8
+print(m.ceil(n))
+print(m.floor(n))
+print(round(n)) #內建不須打math
+
+
+
+
+
---
+++ PythonProject/module_intro1.py
@@ -0,0 +1,3 @@
+#直接調用模組,不用import
+from math import pi
+print(pi)
