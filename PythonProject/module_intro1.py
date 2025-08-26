6039e75956b9d496974a8d60dab42ff11a1c6811
Jasper <jasperc1972@gmail.com>
2025-08-26 10:15:25 +0800

    Calling module methods
    Calling module methods

--- PythonProject/module_intro1.py
+++ PythonProject/module_intro1.py
@@ -1,3 +1,8 @@
+import my_module as m1
+print(m1.area(2))
+print(m1.cube(3))
+print(m1.square(4))
+
 #直接調用模組,不用import
 from math import pi
 print(pi)
---
+++ PythonProject/my_module.py
@@ -0,0 +1,12 @@
+
+
+pi = 3.14
+def square(x):
+    return x**2
+def cube(x):
+    return x**3
+
+def area(radius):
+    return pi*radius**2
+def perimeter(radius):
+    return 2*pi*radius
